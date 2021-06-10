437a0495892bd8a290f4911a8dc2f3dd2539b3ba
MYCOMPUTER\yuit0 <ogawatakafumi2814@gmail.com>
2021-06-10 17:03:40 +0900

    popup background, app bar app name

--- app/src/main/java/zekki/kaihi/alarm/MainActivity.java
+++ app/src/main/java/zekki/kaihi/alarm/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("");
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
