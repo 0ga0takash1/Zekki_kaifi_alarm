dd31b037dd8562407fa7143ed5bdd9581436c1b3
MYCOMPUTER\yuit0 <ogawatakafumi2814@gmail.com>
2021-09-19 20:52:15 +0900

    プレゼン用

--- app/src/main/java/zekki/kaihi/alarm/MainActivity.java
+++ app/src/main/java/zekki/kaihi/alarm/MainActivity.java
@@ -41,12 +41,6 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
 
-        // アラームのデータを取得
-        ArrayList<AlarmInfo> data = this.loadAlarms();
-
-        // RecyclerViewに設定
-        this.setRV(data);
-
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,82 +72,10 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(getApplication(), DeleteActivity.class);
             startActivity(intent);
         } else if (item.getItemId() == R.id.action_license) {
-            CreateLicence();
         }
 
         return super.onOptionsItemSelected(item);
     }
 
 
-    private ArrayList<AlarmInfo> loadAlarms(){
-        helper = DatabaseHelper.getInstance(this);
-
-        ArrayList<AlarmInfo> data = new ArrayList<>();
-
-        try(SQLiteDatabase db = helper.getReadableDatabase()) {
-
-            String[] cols ={"alarmid","name","alarttime"};
-
-            Cursor cs = db.query("alarms",cols,null,null,
-                    null,null,"alarmid",null);
-            boolean eol = cs.moveToFirst();
-            while (eol){
-                AlarmInfo item = new AlarmInfo();
-                item.setAlarmID(cs.getInt(0));
-                item.setAlarmName(cs.getString(1));
-                item.setTime(cs.getString(2));
-                data.add(item);
-                eol = cs.moveToNext();
-            }
-        }
-        return data;
-    }
-
-    private void setRV(ArrayList<AlarmInfo> data){
-        rv = (RecyclerView)findViewById(R.id.rv);
-        rv.setHasFixedSize(true);
-        LinearLayoutManager manager = new LinearLayoutManager(this);
-        manager.setOrientation(LinearLayoutManager.VERTICAL);
-        rv.setLayoutManager(manager);
-        adapter = new ListAdapter(data);
-        rv.setAdapter(adapter);
-    }
-
-    private void updateRV(ArrayList<AlarmInfo> data){
-        adapter = new ListAdapter(data);
-        rv.setAdapter(adapter);
-        // rv.swapAdapter(adapter,false);
-    }
-
-    private void CreateLicence() {
-        // PopupWindow内に表示するFrameLayoutを作成する。
-        FrameLayout contentView = new FrameLayout(this);
-        contentView.setBackgroundColor(Color.BLACK);
-
-        // PopupWindowのインスタンスを作成する
-        PopupWindow popupWindow = new PopupWindow();
-        // PopupWindow内に表示するViewを設定する。
-        popupWindow.setContentView(contentView);
-        // PopupWindowの幅を800に設定
-        popupWindow.setWidth(10);
-        // PopupWindowの高さを800に設定
-        popupWindow.setHeight(10);
-        // PopupWindowを消すボタンを作成
-        Button dismissPopupWindow = new Button(this);
-        dismissPopupWindow.setText("PopupWindowを消す");
-        dismissPopupWindow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // PopupWindow#dismissでPopupWindowを消す
-                popupWindow.dismiss();
-            }
-        });
-
-        LinearLayout linearLayout = new LinearLayout(this);
-        linearLayout.addView(dismissPopupWindow);
-
-        FrameLayout frameLayout = new FrameLayout(this);
-        frameLayout.addView(linearLayout);
-        setContentView(frameLayout);
-    }
 }
