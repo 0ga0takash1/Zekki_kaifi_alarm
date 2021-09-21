4240b2b34f9c16ef58b6b5413ae9cbd58d6515ae
MYCOMPUTER\yuit0 <ogawatakafumi2814@gmail.com>
2021-09-21 17:48:01 +0900

    プレゼン用

--- app/src/main/java/zekki/kaihi/alarm/InputSubAlarmFragment.java
+++ app/src/main/java/zekki/kaihi/alarm/InputSubAlarmFragment.java
@@ -2,11 +2,13 @@ package zekki.kaihi.alarm;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,5 +62,11 @@ public class InputSubAlarmFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_input_sub_alarm, container, false);
+    }
+
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button sub_back = view.findViewById(R.id.sub_alarm_background);
+        sub_back.setEnabled(false);
     }
 }
